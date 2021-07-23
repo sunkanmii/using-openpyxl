6a212559c6093b580ff05fad88df476533c3c0f4
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-07-23 01:13:55 +0100

    delete active property from workbook object - not needed

--- automateExcel.py
+++ automateExcel.py
@@ -1,10 +1,11 @@
 import openpyxl
 
-firstFile = r"C:\\Users\Sunkanmi-PC\Documents\newFile1.xlsx"
-secondFile = r"C:\\Users\Sunkanmi-PC\Documents\newFile2.xlsx"
+firstFile = r"C:\\Users\Sunkanmi-PC\Documents\company-in-nigeria.xlsx"
+secondFile = r"C:\\Users\Sunkanmi-PC\Documents\company-in-rwanda.xlsx"
 
-wbObj1 = openpyxl.load_workbook(firstFile.strip())    
-wbObj2 = openpyxl.load_workbook(secondFile.strip()) 
+wbObj1 = openpyxl.load_workbook(firstFile)    
+wbObj2 = openpyxl.load_workbook(secondFile)
+
 wbk1 = wbObj1.worksheets[0]
 wbk2 = wbObj2.worksheets[0]
 
@@ -12,15 +13,15 @@ wbk2 = wbObj2.worksheets[0]
 sheetObj1 = wbObj1.active
 sheetObj2 = wbObj2.active
 
-hostNames = []
-for myRow in range(2, sheetObj2.max_row):
-    hostNames.append(sheetObj2.cell(row=myRow, column=1).value.lower())
+names = []
+for myRow in range(2, wbk2.max_row + 1):
+    names.append(wbk2.cell(row=myRow, column=1).value.lower())
 wbObj2.close
 
-for rowNum in range(2, sheetObj1.max_row):
-    if wbk1.cell(row=rowNum, column=1).value.lower() in hostNames:
-	    wbk1.cell(row=rowNum, column=2).value = "Yes"
+for rowNum in range(2, wbk1.max_row + 1):
+    if wbk1.cell(row=rowNum, column=1).value.lower() in names:
+	    wbk1.cell(row=rowNum, column=2).value = "Present"
     else:
-	    wbk1.cell(row=rowNum, column=2).value = "Not on Sophos"
+	    wbk1.cell(row=rowNum, column=2).value = "Not present"
 wbObj1.save(firstFile)
 wbObj1.close
